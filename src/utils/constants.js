df4f2230b9d0cf67233cf8e577f69cb9fb058f84
phamlong <phamlong123np@gmail.com>
2025-07-14 20:57:50 +0700

    Update CROS not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 //Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_ORIGIN = [
-  'http://localhost:5173'
+  // 'http://localhost:5173'//ko cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev
 ]
 
 export const BOARD_TYPES = {
