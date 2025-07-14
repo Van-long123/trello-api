57a57febf8fc223ee6e8a7f1cf6851e0856dd8e8
phamlong <phamlong123np@gmail.com>
2025-07-14 23:32:19 +0700

    Update Whitelist Origin

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 //Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_ORIGIN = [
   // 'http://localhost:5173'//ko cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev
+  'https://trello-web-three-pink.vercel.app'
 ]
 
 export const BOARD_TYPES = {
