c13055d85acdeb24982d49b7ffa04a187f3c8190
phamlong <phamlong123np@gmail.com>
2025-08-01 14:44:13 +0700

    Create, Get Sheet Details, Update Column Headers Features

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -15,8 +15,9 @@ const getBoards = async (req, res, next) => {
 
 const createNew = async (req, res, next) => {
   try {
+    const userId = req.jwtDecoded._id
     // Điều hướng dữ liệu sang tầng service
-    const createdBoard = await boardService.createNew(req.body)
+    const createdBoard = await boardService.createNew(userId, req.body)
 
     res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) {
@@ -28,8 +29,9 @@ const createNew = async (req, res, next) => {
 }
 const getDetails = async (req, res, next) => {
   try {
+    const userId = req.jwtDecoded._id
     const boardId = req.params.id
-    const board = await boardService.getDetails(boardId)
+    const board = await boardService.getDetails(userId, boardId)
 
     res.status(StatusCodes.OK).json(board)
   } catch (error) {
